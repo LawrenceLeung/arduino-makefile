4518b106137c743aee2bd918cac6330c36705057
Camille Moncelier <moncelier@devlife.org>
2011-04-03 18:50:06 +0200

    Created a Makefile for each targets (Arduino/Teensy)

--- scripts/make.rb
+++ scripts/make.rb
@@ -89,47 +89,24 @@ OBJCP = avr-objcopy
 SIZE = avr-size -A --mcu=$(MCU)
 AVRDUDE = avrdude
 
+UPLOAD_RATE = 19200
+F_CPU = 16000000
+FORMAT = ihex
+USB=0
+
 LIBRARIES = $(TARGET_BOARD).a #{(Libs.collect { |x| x.name } - (SupportedTargets + IgnoredLibs).collect { |x| x + ".a" }).join(" ")} 
 OBJECTS = #{Sources.objects.join(" ")}
 INCLUDES = -I"/usr/lib/avr/include/avr" -I"./include" #{Headers.join(" ")} -I"lib/$(TARGET_BOARD)"
 DEFINES = -DF_CPU=$(F_CPU)L -DARDUINO=18
 
-TEENSY_KEYBOARD_LAYOUT = LAYOUT_US_INTERNATIONAL
-TEENSY_FLAGS =
-TEENSY_USB = usb_hid
 CPP_FLAGS = -g -Os -w -fno-exceptions -ffunction-sections -fdata-sections -mmcu=$(MCU) $(DEFINES) $(INCLUDES)
 LD_FLAGS = -Os -Wl,--gc-sections -mmcu=$(MCU)
 AR_FLAGS = rcs
 
-
-USB=0
-PORT = /dev/ttyUSB$(USB)
-MCU = atmega328p
-F_CPU = 16000000
-FORMAT = ihex
-UPLOAD_RATE = 19200
-MAX_SIZE = 30720
+default: all
 
 include Makefile.local
-
-ifeq ($(TARGET_BOARD),teensy)
-  CPP_FLAGS += $(TEENSY_FLAGS) -D$(TEENY_KEYBOARD_LAYOUT)=1
-  LIBRARIES += $(TEENSY_USB).a
-
-  ifeq ($(TEENSY_USB),usb_serial)
-    CPP_FLAGS += -DUSB_SERIAL
-  endif
-  ifeq ($(TEENSY_USB),usb_midi)
-    CPP_FLAGS += -DUSB_MIDI
-  endif
-  ifeq ($(TEENSY_USB),usb_disk)
-    CPP_FLAGS += -DUSB_DISK
-  endif
-  ifeq ($(TEENSY_USB),usb_hid)
-    CPP_FLAGS += -DUSB_HID
-  endif
-endif
-
+include targets/Makefile.$(TARGET_BOARD)
 
 OUTPUT = $(PROJECT_NAME)
 
@@ -148,9 +125,8 @@ $(OUTPUT).hex: $(OUTPUT).elf
   
 .PHONY: upload clean
 
-upload: all
-\tstty -F $(PORT) hupcl
-\t$(AVRDUDE) -V -F -p $(MCU) -c stk500v1 -P $(PORT) -b $(UPLOAD_RATE) -D -Uflash:w:$(OUTPUT).hex:i 
+upload: all upload_pre
+\t$(AVRDUDE) -V -F -p $(MCU) -c $(PROGRAMMER) -P $(PORT) -b $(UPLOAD_RATE) -D -Uflash:w:$(OUTPUT).hex:i $(AVRDUDE_MISC)
 
 clean:
 \t@rm -f $(LIBRARIES) $(OUTPUT).elf $(OUTPUT).hex $(shell find . -follow -name "*.o")
